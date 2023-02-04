3a7fde04d967d71f3e4fac4a4c72107fbae96285
jigargosar <jigar.gosar@gmail.com>
2023-02-04 12:49:25 +0530

    circles and curves

--- src/E4CirclesEllipsesLissajousCurves.elm
+++ src/E4CirclesEllipsesLissajousCurves.elm
@@ -2,9 +2,9 @@ module E4CirclesEllipsesLissajousCurves exposing (main)
 
 import Browser
 import Browser.Events
-import Html exposing (Html)
+import Html exposing (Html, button, div, text)
 import Html.Attributes exposing (style)
-import Svg exposing (circle, g, svg)
+import Svg exposing (circle, foreignObject, g, svg)
 import Svg.Attributes exposing (cx, cy, fill, r)
 
 
@@ -75,32 +75,41 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    svg
-        [ style "background-color" "black"
-        , style "display" "flex"
+    div
+        [ style "display" "grid"
         , style "width" "100vw"
         , style "height" "100vh"
-        , fill "white"
         ]
-        [ g [ style "transform" "translate(50%,50%)" ]
-            [ let
-                xRadius =
-                    150
+        [ div
+            [ style "position" "fixed"
+            ]
+            [ button [] [ text "toggle" ] ]
+        , svg
+            [ style "background-color" "black"
+            , style "width" "100%"
+            , style "height" "100%"
+            , fill "white"
+            ]
+            [ g [ style "transform" "translate(50%,50%)" ]
+                [ let
+                    xRadius =
+                        150
 
-                yRadius =
-                    200
+                    yRadius =
+                        200
 
-                x =
-                    cos model.xAngle * xRadius
+                    x =
+                        cos model.xAngle * xRadius
 
-                y =
-                    sin model.yAngle * yRadius
-              in
-              circle
-                [ cx (String.fromFloat x)
-                , cy (String.fromFloat y)
-                , r "1%"
+                    y =
+                        sin model.yAngle * yRadius
+                  in
+                  circle
+                    [ cx (String.fromFloat x)
+                    , cy (String.fromFloat y)
+                    , r "1%"
+                    ]
+                    []
                 ]
-                []
             ]
         ]
