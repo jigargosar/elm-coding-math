d503fa74982d32433f4a72700739f031cc1d4faf
jigargosar <jigar.gosar@gmail.com>
2023-02-04 12:12:21 +0530

    circles and curves

--- src/E4CirclesEllipsesLissajousCurves.elm
+++ src/E4CirclesEllipsesLissajousCurves.elm
@@ -1,11 +1,39 @@
 module E4CirclesEllipsesLissajousCurves exposing (main)
 
+import Browser
+import Html exposing (Html)
 import Html.Attributes exposing (style)
 import Svg exposing (circle, g, svg)
 import Svg.Attributes exposing (fill, r)
 
 
 main =
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+type alias Model =
+    { angle : Float
+    }
+
+
+init : Model
+init =
+    { angle = 0 }
+
+
+type Msg
+    = Msg
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Msg ->
+            model
+
+
+view : Model -> Html Msg
+view _ =
     svg
         [ style "background-color" "black"
         , style "display" "flex"
